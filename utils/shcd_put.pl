2221ad4c93a835a8154f6b4828a9db2720512d41
Andrea Guzzo <andrea.guzzo@booking.com>
2013-12-04 16:26:17 +0100

    updated the utils to make use of the new __index__ and __stats__ pages

--- utils/shcd_put.pl
+++ utils/shcd_put.pl
@@ -4,9 +4,9 @@ use strict;
 use HTTP::Request;
 use LWP;
 use File::Slurp;
-use DBI;
 
-my $dbfile = $ENV{SHCD_DBFILE} || "$ENV{HOME}/shd.db";
+$ENV{PERL_LWP_ENV_PROXY} = 1
+    if ($ENV{http_proxy});
 
 my $input_file = shift @ARGV;
 my $key = shift @ARGV;
@@ -17,12 +17,6 @@ die "Usage: $0 </path/to/input_file> <key> [<hosts>]" unless($key && $input_file
 my @hosts_array = split(',', $hosts);
 my $host = $hosts_array[int(rand(scalar(@hosts_array)))];
 
-my $dbh = DBI->connect("dbi:SQLite:dbname=$dbfile","","")
-    or die "Can't open sqlite database $dbfile";
-
-$dbh->do("CREATE TABLE IF NOT EXISTS shd_index (key TEXT PRIMARY KEY, size INTEGER)");
-
-
 print "Using host $host : ";
  
 my $data = read_file($input_file);
@@ -31,9 +25,6 @@ my $request = HTTP::Request->new("PUT", "http://$host/$key", $req_headers, $data
 
 my $ua = LWP::UserAgent->new;
 my $response = $ua->request($request);
-if ($response->code == 200) {
-    $dbh->do(sprintf"INSERT OR REPLACE INTO shd_index VALUES(%s, %d)", $dbh->quote($key), length($data));
-}
 printf "%s : %s\n",  $response->code, $response->message;
 
 
--- utils/shcd_stats.pl
+++ utils/shcd_stats.pl
@@ -12,15 +12,16 @@ my $hosts = shift @ARGV || $ENV{SHCD_HOSTS};
 my @hosts_array = split(',', $hosts);
 
 foreach my $host (@hosts_array) {
-    print "** Keys on host $host\n\n";
-    my $request = HTTP::Request->new("GET", "http://$host/__index__?nohtml=1");
+    print "** Counters from host $host\n\n";
+    my $request = HTTP::Request->new("GET", "http://$host/__stats__?nohtml=1");
     my $ua = LWP::UserAgent->new;
     my $response = $ua->request($request);
     my $data = $response->content;
     my @lines = split("\r\n", $data);
     foreach my $line (@lines) {
-        my ($key, $length) = split(';', $line);
-        printf("%-64.256s %s\n", $key, $length);
+        my ($key, $value) = split(';', $line);
+        next if ($key =~ /^worker\[/ && $value == 0);
+        printf("%-64.256s %s\n", $key, $value);
     }
     print "\n";
 }
