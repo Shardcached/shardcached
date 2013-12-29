00dd6e034f81f3399b32167ec1d7874c6048e046
xant <xant@xant.net>
2013-12-29 18:38:55 +0100

    documentation

--- src/storage.h
+++ src/storage.h
@@ -6,9 +6,16 @@
 #define MAX_STORAGE_OPTIONS 256
 #define MAX_OPTIONS_STRING_LEN 2048
 
+// 
+// Check <src_base>/storage_plugins/README
+// for detaile about the Loadable Storage Modules API
+//
+
 typedef struct shcd_storage_s shcd_storage_t;
 
-shcd_storage_t * shcd_storage_init(char *storage_type, char *options_string, char *plugins_dir);
+shcd_storage_t * shcd_storage_init(char *storage_type,
+                                   char *options_string,
+                                   char *plugins_dir);
 
 void shcd_storage_destroy(shcd_storage_t *st);
 
