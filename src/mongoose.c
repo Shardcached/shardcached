1776716e0572cdc2bfa64a131f8b27850b569644
Andrea Guzzo <andrea.guzzo@booking.com>
2014-01-24 13:42:54 +0100

    commented out some assignments leading to "dead store"
    reported by the static analyzer

--- src/mongoose.c
+++ src/mongoose.c
@@ -1135,7 +1135,7 @@ static void open_cgi_endpoint(struct connection *conn, const char *prog) {
     *p++ = '\0';
   } else {
     dir[0] = '.', dir[1] = '\0';
-    p = (char *) prog;
+    //p = (char *) prog;
   }
 
   // Try to create socketpair in a loop until success. mg_socketpair()
@@ -1486,7 +1486,7 @@ static int match_prefix(const char *pattern, int pattern_len, const char *str) {
   }
 
   i = j = 0;
-  res = -1;
+  //res = -1;
   for (; i < pattern_len; i++, j++) {
     if (pattern[i] == '?' && str[j] != '\0') {
       continue;
@@ -1700,7 +1700,7 @@ static void SHA1Transform(uint32_t state[5], const unsigned char buffer[64]) {
   // Erase working structures. The order of operations is important,
   // used to ensure that compiler doesn't optimize those out.
   memset(block, 0, sizeof(block));
-  a = b = c = d = e = block[0].l[0];
+  //a = b = c = d = e = block[0].l[0];
 }
 
 static void SHA1Init(SHA1_CTX* context) {
