bdb0b31a0699a123a332a8294a4219bd82860bbf
Andrea Guzzo <andrea.guzzo@booking.com>
2014-04-01 10:44:51 +0200

    version bump + updated dependencies

--- src/shardcached.c
+++ src/shardcached.c
@@ -33,7 +33,7 @@
 #include "storage.h"
 #include "ini.h"
 
-#define SHARDCACHED_VERSION "0.10"
+#define SHARDCACHED_VERSION "0.11"
 
 #define SHARDCACHED_ADDRESS_DEFAULT "4321"
 #define SHARDCACHED_LOGLEVEL_DEFAULT 0
